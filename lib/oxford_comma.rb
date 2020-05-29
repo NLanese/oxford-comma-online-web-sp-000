1f42fcd4e915d207d99e7d034e7259f20dfedcc6
Nick Lanese <nlanese21@gmail.com>
2020-05-29 19:21:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,18 @@
 def oxford_comma(array)
-  point1 = array.length() - 3
-  point2 = array.length() - 2
-  i = 0
-  until i == point1
-    print ("#{array[i]}, ")
-    i = i + 1
+  if (array.length == 1)
+    puts array[0]
+  elsif (array.length == 2)
+    puts "#{array[0]} and #{array[1]}"
+  else
+    point1 = array.length() - 3
+    point2 = array.length() - 2
+    i = 0
+    until i == point1
+      print ("#{array[i]}, ")
+      i = i + 1
+    end
+    print ("#{array[i]} and ")
+    i + 1
+    print array[i]
   end
-  print ("#{array[i]} and ")
-  i + 1
-  print array[i]
 end
