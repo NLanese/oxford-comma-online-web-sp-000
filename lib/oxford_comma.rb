5ed01db3d382e72e248560bce0c42434aea3b434
Nick Lanese <nlanese21@gmail.com>
2020-05-29 19:07:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,8 @@
 def oxford_comma(array)
-  point1 = array.length() - 2
+  point1 = array.length() - 3
+  point2 = array.length() - 2
   i = 0
-  until i == len - 1
+  until i == point1
     print array[i] + ", "
     i = i + 1
   end
